211b0c8caa5b19fadb02fb13756cf7a12675bd76
G-Sampaio <109550791+G-Sampaio@users.noreply.github.com>
2023-06-26 14:53:26 -0300

    Update main.java

--- main.java
+++ main.java
@@ -2,18 +2,27 @@ package aplicacao;
 
 import sublime_main.Jogador;
 import sublimeDAO.jogadorDAO;
+import java.util.Scanner;
 
 
 public class main {
-
+	private static Scanner input= new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		
+		Scanner s = new Scanner(System.in);
 		Jogador jogador= new Jogador();		
 		jogadorDAO jogadorDao = new jogadorDAO();
 		
-		jogador.setNome("Guilherme Sampaio");
-		jogador.setNickname("Delta");		
-		//jogadorDao.salvar(jogador);
+		System.out.print("DIGITE O NOME DO JOGADOR: ");
+		
+		String nome = input.nextLine();
+		jogador.setNome(nome);
+		
+		System.out.println("DIGITE O APELIDO DO JOGADOR");
+		String nickname = input.nextLine();
+		
+		jogador.setNickname(nickname);		
+		jogadorDao.salvar(jogador);
 		
 		// Atualizar contato
 		
@@ -26,18 +35,18 @@ public class main {
 		
 		//Deletar Contato por id
 		
-		jogadorDAO.deletar(2);
+		//jogadorDAO.deletar(2);
 		
 		//Visualizar Registros
 		
-		for(Jogador j : jogadorDAO.getJogador()) {
-			System.out.printf("\nJogador: "+j.getNome() + " || Apelido: "+j.getNickname());
+		//for(Jogador j : jogadorDAO.getJogador()) {
+			//System.out.printf("\nJogador: "+j.getNome() + " || Apelido: "+j.getNickname());
 		
 		
 			
 			
 			
-		}
+		//}
 		
 		
 		
