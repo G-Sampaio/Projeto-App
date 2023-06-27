1edb36ebe01eab778677d26be620496451902c56
G-Sampaio <109550791+G-Sampaio@users.noreply.github.com>
2023-06-26 21:56:59 -0300

    Add files via upload

---
+++ pontuacao.java
@@ -0,0 +1,21 @@
+package sublime_main;
+
+public class pontuacao {
+	
+	private String inserir_pontuacao;
+	private String mostrar_pontuacao;
+	
+	
+	public String getMostrar_pontuacao() {
+		return mostrar_pontuacao;
+	}
+	public void setMostrar_pontuacao(String mostrar_pontuacao) {
+		this.mostrar_pontuacao = mostrar_pontuacao;
+	}
+	public String getInserir_pontuacao() {
+		return inserir_pontuacao;
+	}
+	public void setInserir_pontuacao(String inserir_pontuacao) {
+		this.inserir_pontuacao = inserir_pontuacao;
+	}
+}
