a3e127dfcf8ded039f56830241448a79e0ea3e12
G-Sampaio <109550791+G-Sampaio@users.noreply.github.com>
2023-06-26 21:44:58 -0300

    Create jogo.java

---
+++ jogo.java
@@ -0,0 +1,38 @@
+package sublime_main;
+
+public class Jogo {
+	
+	private String nome_jogo;
+	private String tema;
+	private Integer id;
+	private Integer pontMax;
+	
+	
+	public String getNome_jogo() {
+		return nome_jogo;
+	}
+	public void setNome_jogo(String nome_jogo) {
+		this.nome_jogo = nome_jogo;
+	}
+	public String getTema() {
+		return tema;
+	}
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getPontMax() {
+		return pontMax;
+	}
+	public void setPontMax(Integer pontMax) {
+		this.pontMax = pontMax;
+	}
+
+	
+	
+}
