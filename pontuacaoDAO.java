288a54ea242c637b15d437af716da34f53110073
G-Sampaio <109550791+G-Sampaio@users.noreply.github.com>
2023-06-29 00:42:32 -0300

    Update pontuacaoDAO.java

--- pontuacaoDAO.java
+++ pontuacaoDAO.java
@@ -2,8 +2,12 @@ package sublimeDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import db_pessoas.ConnectionFactory;
+
 import sublime_main.pontuacao;
 
 public class pontuacaoDAO {
@@ -30,6 +34,45 @@ public class pontuacaoDAO {
 		}
 	}
 	
-	
+		
+	public static List<pontuacao> getMostrarPontuacao(String nome_jgr, String nome_jg) {
+	    
+		String sql = "SELECT pontuacao\r\n"
+	    		+ "FROM pontuacao AS pt\r\n"
+	    		+ "JOIN jogador as jgr on pt.nomejgr = jgr.id\r\n"
+	    		+ "join jogo as jg on pt.nomejg = jg.id\r\n"
+	    		+ "where jgr.nome = ? and jg.nome = ?;";
+
+	    List<pontuacao> pontuacoes = new ArrayList<>();
+	    
+	    Connection conn = null;
+	    PreparedStatement pstm = null;
+	    ResultSet rset = null;
+
+	    try {
+	        conn = ConnectionFactory.ObtemConexao();
+	        pstm = conn.prepareStatement(sql);
+	        
+	        pstm.setString(1, nome_jgr);
+	        pstm.setString(2, nome_jg);
+	        
+	        rset = pstm.executeQuery();
+
+	        while (rset.next()) {
+	        	pontuacao Pontuacao = new pontuacao();
+
+	            Pontuacao.setInserir_pontuacao(rset.getString("pontuacao"));
+	            pontuacoes.add(Pontuacao);
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+		return pontuacoes;
+			
+
+				
+			}
+		}
+		
 	
 }
